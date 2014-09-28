d5292250c2d38118a0007c143bc437b532e86b9b
pestretsov <pestretsov@me.com>
2014-09-28 17:34:34 +0400

    fix

--- courses.js
+++ courses.js
@@ -1,16 +1,21 @@
 var courses = [{
     title: "Algorithms",
+    description: "algo",
     year: 2014
 }, {
     title: "Patterns",
+    description: "patt",
     year: 2014
 }, {
     title: "Arduino",
+    description: "ard",
     year: 2013
 }, {
     title: "Math",
+    description: "kosj",
     year: 2014
 }, {
     title: "Java",
+    description: "JVM",
     year: 2014
 }];
