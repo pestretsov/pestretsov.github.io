b3c95aee6e094ebd6499e4524bd674bc122df2cd
pestretsov <pestretsov@me.com>
2014-09-28 15:34:04 +0400

    making scrollable lists

--- courses.js
+++ courses.js
@@ -5,6 +5,12 @@ var courses = [{
     title: "Patterns",
     year: 2014
 }, {
+    title: "Arduino",
+    year: 2013
+}, {
     title: "Math",
+    year: 2014
+}, {
+    title: "Java",
     year: 2014
 }];
