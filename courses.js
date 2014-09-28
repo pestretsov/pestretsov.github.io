1bad6d05cb43c1f683c053b8d0e06110560327d0
pestretsov <pestretsov@me.com>
2014-09-28 20:19:48 +0400

    add buttons and css

--- courses.js
+++ courses.js
@@ -1,21 +1,31 @@
 var courses = [{
     title: "Algorithms",
-    description: "algo",
+    description: "Курс знакомит слушателей с базовыми алгоритмами (метод «разделяй и властвуй», поиск в массиве, сортировка массива, алгоритмы на графах) и структурами данных (расширяющийся массив, куча, дерево отрезков, системы непересекающихся множеств).",
+    level: 5,
     year: 2014
 }, {
     title: "Patterns",
-    description: "patt",
+    description: "Без Димона и паттернов жизнь не та всегда",
+    level: 4,
     year: 2014
 }, {
     title: "Arduino",
-    description: "ard",
+    description: "Палим ебучие схемы. Нихера не работает. Левые препод",
+    level: 4,
     year: 2013
 }, {
     title: "Math",
-    description: "kosj",
+    description: "Хуярим еще больше матана, БЛЯДБ",
+    level: 4,
     year: 2014
 }, {
     title: "Java",
-    description: "JVM",
+    description: "НА кой хуй мы выбрали жаву, кагда есть питон и плюсы? хуй знает",
+    level: 3,
     year: 2014
+}, {
+    title: "Test",
+    description: "TestTestTestTestTestTestTestTestTestTestTestTestTest",
+    level: 2,
+    year: 2011
 }];
