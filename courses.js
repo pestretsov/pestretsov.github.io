7770dece45752033fc77c061f52da54a8f897680
Artemy Pestretsov <pestretsov@me.com>
2014-10-03 03:55:25 +0400

    buggy

--- courses.js
+++ courses.js
@@ -1,31 +1,37 @@
 var courses = [{
+    CourseID: 1,
     title: "Algorithms",
-    description: "Курс знакомит слушателей с базовыми алгоритмами (метод «разделяй и властвуй», поиск в массиве, сортировка массива, алгоритмы на графах) и структурами данных (расширяющийся массив, куча, дерево отрезков, системы непересекающихся множеств).",
-    level: 5,
-    year: 2014
+    description: "Курс знакомит слушателей с базовыми алгоритмами (метод «разделяй и властвуй», поиск в массиве, сортировка массива, алгоритмы на графах) и структурами данных (расширяющийся массив, куча, дерево отрезков, системы непересекающихся множеств)."
+//    level: 5,
+//    year: 2014
 }, {
-    title: "Patterns",
-    description: "Без Димона и паттернов жизнь не та всегда",
-    level: 4,
-    year: 2014
-}, {
-    title: "Arduino",
-    description: "Палим ебучие схемы. Нихера не работает. Левые препод",
-    level: 4,
-    year: 2013
-}, {
-    title: "Math",
-    description: "Хуярим еще больше матана, БЛЯДБ",
-    level: 4,
-    year: 2014
-}, {
-    title: "Java",
-    description: "НА кой хуй мы выбрали жаву, кагда есть питон и плюсы? хуй знает",
-    level: 3,
-    year: 2014
-}, {
+    CourseID: 2,
     title: "Test",
-    description: "TestTestTestTestTestTestTestTestTestTestTestTestTest",
-    level: 2,
-    year: 2011
+    description: "TestTestTestTestTestTestTestTestTestTestTestTestTest"
+//    level: 2,
+//    year: 2014
+}, {
+    CourseID: 3,
+    title: "Test",
+    description: "TestTestTestTestTestTestTestTestTestTestTestTestTest"
+//    level: 2,
+//    year: 2011
+}, {
+    CourseID: 4,
+    title: "Test",
+    description: "TestTestTestTestTestTestTestTestTestTestTestTestTest"
+//    level: 2,
+//    year: 2011
+}, {
+    CourseID: 5,
+    title: "Test",
+    description: "TestTestTestTestTestTestTestTestTestTestTestTestTest"
+//    level: 2,
+//    year: 2011
+}, {
+    CourseID: 6,
+    title: "Test",
+    description: "TestTestTestTestTestTestTestTestTestTestTestTestTest"
+//    level: 2,
+//    year: 2011
 }];
